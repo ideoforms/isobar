a6cf6095f08054d1e13014d509987d0109ddfba1
Daniel Jones <d.jones@gold.ac.uk>
2011-09-07 09:17:08 +0100

    updated docs, added subsequence example

---
+++ examples/ex-subsequence.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from isobar import *
+from isobar.io.midiout import *
+
+scale = Scale([ 0, 2, 3, 6, 7 ])
+scale = Scale.pelog
+seq = PDegree(PBrown(0, 3, -12, 12, repeats = False), scale)
+
+offset = PStutter(PWhite(0, 4), 2)
+seq = PSubsequence(seq, offset, 4)
+# seq = PLoop(seq, 3)
+seq = PPermut(seq)
+seq = seq + 64
+seq = PReset(seq, PImpulse(24))
+
+amp = PSeq([ 45, 35, 25, 40 ]) + PBrown(0, 1, -15, 10)
+amp = PSeq([ 45, 35, 35, 40 ]) + PBrown(0, 1, -15, 10)
+
+for n in range(25):
+	print seq.next()
+
+gate = PBrown(1.5, 0.01, 0.6, 2.5)
+
+timeline = Timeline(120)
+midi = MidiOut()
+timeline.output(midi)
+timeline.sched({ 'note': seq, 'amp': amp, 'dur': 0.25, 'gate': gate })
+timeline.sched({ 'note': seq.copy() + 24, 'amp': amp.copy(), 'dur': 0.5, 'gate': gate.copy() })
+timeline.sched({ 'note': seq.copy() - 24, 'amp': 10 + amp.copy() * 0.5, 'dur': PChoice([ 4, 4, 6, 8 ]), 'gate': gate.copy() })
+timeline.run()
+
