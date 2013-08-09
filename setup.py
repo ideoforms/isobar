75ba51992cca9cc161f17cb69f425ca2be3daff9
Daniel Jones <daniel@jones.org.uk>
2013-08-09 18:01:04 +0100

    add distutils setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name = 'isobar',
+	version = '0.0.1',
+	description = 'A Python library to express and manipulate musical patterns',
+	author = 'Daniel Jones',
+	author_email = 'dan-isobar@erase.net',
+	url = 'http://www.erase.net/projects/isobar',
+	packages = ['isobar']
+)
