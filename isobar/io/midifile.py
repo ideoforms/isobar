5441803fcd8933b54dd912634f71fb01980c282f
Daniel Jones <daniel@jones.org.uk>
2013-11-02 14:06:38 +0000

    rehaul of io/midifile to enable parsing of chords (outputting a tuple of notes), with specifiable quantize level

--- isobar/io/midifile.py
+++ isobar/io/midifile.py
@@ -10,7 +10,7 @@ class MidiFileIn:
 	def __init__(self):
 		pass
 
-	def read(self, filename):
+	def read(self, filename, quantize = 0.25):
 		reader = midi.FileReader()
 		data = reader.read(file(filename))
 
@@ -30,11 +30,30 @@ class MidiFileIn:
 			track.make_ticks_abs()
 			for event in filter(lambda event: isinstance(event, midi.events.NoteEvent), track):
 				location = event.tick / 96.0
-				if isinstance(event, midi.events.NoteOnEvent):
+				if isinstance(event, midi.events.NoteOnEvent) and event.velocity > 0:
 					print "(%.2f beats) %s. \t(note = %d, velocity = %d)" % (location, miditoname(event.pitch), event.pitch, event.velocity)
+
+					#------------------------------------------------------------------------
+					# anomaly: sometimes a midi file might have a note on when the previous
+					# note has not finished; end it automatically
+					#------------------------------------------------------------------------
+					for note in reversed(notes):
+						if note.pitch == event.pitch:
+							if not note.duration:
+								print "note-on found without note-off; cancelling previous note"
+								duration = location - note.location
+								note.duration = duration
+								if duration == 0:
+									print "*** DURATION OF ZERO??"
+							break
+
 					note = Note(event.pitch, event.velocity, location)
 					notes.append(note)
-				if isinstance(event, midi.events.NoteOffEvent):
+
+				#------------------------------------------------------------------------
+				# A NoteOn event with velocity == 0 also acts as a NoteOff.
+				#------------------------------------------------------------------------
+				if isinstance(event, midi.events.NoteOffEvent) or (isinstance(event, midi.events.NoteOnEvent) and event.velocity == 0):
 					print "(%.2f beats) %s off \t(note = %d, velocity = %d)" % (location, miditoname(event.pitch), event.pitch, event.velocity)
 					found = False
 					for note in reversed(notes):
@@ -46,14 +65,63 @@ class MidiFileIn:
 							break
 					if not found:
 						print "*** NOTE-OFF FOUND WITHOUT NOTE-ON ***"
-	
-		d = {
-			"note" : [ note.pitch for note in notes ],
-			"amp" : [ note.velocity for note in notes ],
-			"dur" : [ note.duration for note in notes ],
+
+		#------------------------------------------------------------------------
+		# Construct a sequence which honours chords and relative lengths.
+		# First, group all notes by their starting time.
+		#------------------------------------------------------------------------
+		notes_by_time = {}
+		for note in notes:
+			print "(%.2f) %d/%d, %s" % (note.location, note.pitch, note.velocity, note.duration)
+			location = note.location
+			if quantize is not None:
+				location = round(location / quantize) * quantize
+			if location in notes_by_time:
+				notes_by_time[location].append(note)
+			else:
+				notes_by_time[location] = [ note ]
+
+		note_dict = {
+			"note" : [],
+			"amp" :  [],
+			"gate" : [],
+			"dur" :  []
 		}
 
-		return d
+		#------------------------------------------------------------------------
+		# Next, iterate through groups of notes chronologically, figuring out
+		# appropriate parameters for duration (eg, inter-note distance) and
+		# gate (eg, proportion of distance note extends across).
+		#------------------------------------------------------------------------
+		times = sorted(notes_by_time.keys())
+		for i in range(len(times)):
+			time = times[i]
+			notes = notes_by_time[time]
+
+			#------------------------------------------------------------------------
+			# Our duration is always determined by the time of the next note event.
+			# If a next note does not exist, this is the last note of the sequence;
+			# use the maximal length of note currently playing (assuming a chord)
+			#------------------------------------------------------------------------
+			if i < len(times) - 1:
+				next_time = times[i + 1]
+			else:
+				next_time = time + max([ note.duration for note in notes ])
+
+			dur = next_time - time
+			note_dict["dur"].append(dur)
+
+			if len(notes) > 1:
+				note_dict["note"].append(tuple(note.pitch for note in notes))
+				note_dict["amp"].append(tuple(note.velocity for note in notes))
+				note_dict["gate"].append(tuple(note.duration / dur for note in notes))
+			else:
+				note = notes[0]
+				note_dict["note"].append(note.pitch)
+				note_dict["amp"].append(note.velocity)
+				note_dict["gate"].append(note.duration / dur)
+
+		return note_dict
 
 class MidiFileOut:
 	def __init__(self, numtracks = 16):
@@ -117,3 +185,4 @@ class PatternWriterMIDI:
 		fd = open(filename, 'wb')
 		self.score.writeFile(fd)
 		fd.close()
+
