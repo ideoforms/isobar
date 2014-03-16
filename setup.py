3181ca14f16b413ab32e4d3f0a75c60eaa0c2f56
Daniel Jones <daniel@jones.org.uk>
2014-03-16 13:27:11 +0000

    expanded setuptools support

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+#------------------------------------------------------------------------
+# Generate a PyPI-friendly RST file from our markdown README.
+#------------------------------------------------------------------------
+try:
+	import pypandoc
+	long_description = pypandoc.convert('README.md', 'rst')
+except:
+	long_description = None
 
 setup(
 	name = 'isobar',
 	version = '0.0.1',
 	description = 'A Python library to express and manipulate musical patterns',
+	long_description = long_description,
 	author = 'Daniel Jones',
 	author_email = 'dan-isobar@erase.net',
-	url = 'http://www.erase.net/projects/isobar',
-	packages = ['isobar']
+	url = 'https://github.com/ideoforms/isobar',
+	packages = ['isobar'],
+	install_requires = ['pyOSC >= 0.3b0'],
+	keywords = ('sound', 'music', 'composition'),
+	classifiers = [
+		'Topic :: Multimedia :: Sound/Audio',
+		'Topic :: Artistic Software',
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers'
+	]
 )
