0bcbaaf223d38da4c6da34e7c12e6d1ae4468564
Daniel Jones <daniel@jones.org.uk>
2014-03-19 13:23:50 +0000

    simplify piano phase example

--- examples/ex-piano-phase.py
+++ examples/ex-piano-phase.py
@@ -11,15 +11,12 @@ from isobar import *
 # melody line
 seq = PSeq([ -7, -5, 0, 2, 3, -5, -7, 2, 0, -5, 3, 2 ])
 
-# fixed duration per note
-dur = PConst(0.5)
-
 timeline = Timeline(160)
 
 # schedule two identical melodies.
 # we must copy the note sequence or else the position will be stepped
 # by two every note... try removing the .copy() and see what happens!
-timeline.sched({ 'note': seq.copy() + 60, 'dur': dur })
-timeline.sched({ 'note': seq.copy() + 72, 'dur': dur * 1.01 })
+timeline.sched({ 'note': seq.copy() + 60, 'dur': 0.5 })
+timeline.sched({ 'note': seq.copy() + 72, 'dur': 0.5 * 1.01 })
 
 timeline.run()
