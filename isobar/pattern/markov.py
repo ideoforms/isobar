e1867d0994a1914e948310419d0f81d228215b95
Daniel Jones <daniel@jones.org.uk>
2015-08-02 22:18:40 +0100

    merge markov chain grapher with markov code

--- isobar/pattern/markov.py
+++ isobar/pattern/markov.py
@@ -1,12 +1,22 @@
 from isobar.pattern import *
 from isobar.util import *
 
+import sys
+
 class PMarkov(Pattern):
-	""" PMarkov: First-order Markov chain.
-	http://pastebin.com/rNu2CSFs
-	TODO: Implement n-order. """
+	""" PMarkov: First-order Markov chain generator.
+	
+	"""
+	# TODO: Implement n-order.
 
 	def __init__(self, nodes = None):
+		""" Create a new Markov chain. 'nodes' can be either be:
+		 * an ordered sequence of notes (which will be used to infer the
+		   probabilities of transitioning between notes), or
+		 * a dictionary of the form { 1 : [ 2, 2, 3 ], 2 : [ 3 ], 3 : [ 1, 2 ] },
+		   where values determine the probability of transitioning from A to B
+		   based on the number of occurrences of B in the list with key A.
+		"""
 		#------------------------------------------------------------------------
 		# avoid using [] (mutable default arguments considered harmful)
 		# http://stackoverflow.com/questions/1132941/least-astonishment-in-python-the-mutable-default-argument
@@ -22,7 +32,7 @@ class PMarkov(Pattern):
 		elif isinstance(nodes, dict):
 			#------------------------------------------------------------------------
 			# Take a dictionary argument with the same format as our internal nodes
-			# model : eg { 1 : [ 2, 3 ], 2 : [ 3 ], 3 : [ 1, 2 ] }
+			# model : eg { 1 : [ 2, 2, 3 ], 2 : [ 3 ], 3 : [ 1, 2 ] }
 			#------------------------------------------------------------------------
 			self.nodes = nodes
 		else:
@@ -56,52 +66,84 @@ class PMarkov(Pattern):
 				print "no such node: %s" % self.node
 
 		if self.node is None:
-			print "PMarkov: got no next node :-("
+			#--------------------------------------------------------------------------------
+			# No node found.
+			# TODO: Handle rests properly (as None values).
+			#--------------------------------------------------------------------------------
+			pass
 
 		return self.node
 
 	@classmethod
-	def fromsequence(self, sequence):
+	def from_sequence(self, sequence):
 		learner = MarkovLearner()
 		for value in sequence:
 			learner.register(value)
 		return PMarkov(learner.markov)
 
-	@classmethod
-	def fromscale(self, scale):
-		# TODO: BROKEN
-		semitones = scale.semitones
-		weights = scale.weights
-		return PMarkov(semitones, [ weights[:] for _ in semitones ])
-
 class MarkovLearner:
+	""" Learn a Markovian sequence by sequentially registering new values
+	and building up a dynamic node graph. """
+
 	def __init__(self):
 		self.markov = PMarkov()
 		self.last = None
+	
+	def get_markov(self):
+		""" Returns the PMarkov pattern generator produced by the learning
+		process. """
+		return self.markov
 
 	def register(self, value):
-		# print "registering %s in %s (%s)" % (value, self.markov, self.markov.nodes)
 		if value not in self.markov.nodes:
 			self.markov.nodes[value] = []
 		if self.last is not None:
 			self.markov.nodes[self.last].append(value)
 		self.last = value
 
-class MarkovLParallel:
+class MarkovParallelLearners:
 	def __init__(self, count):
 		self.count = count
-		self.learners = map(lambda n: MarkovLearner(), range(count))
+		self.learners = [ MarkovLearner() for _ in range(count) ]
 
 	def register(self, list):
 		for n in range(self.count):
-			print "registering %d in %s" % (list[n], self.learners[n])
 			self.learners[n].register(list[n])
 
-	def normalize(self):
-		for learner in self.learners:
-			learner.markov.normalize()
-
 	def chains(self):
-		self.normalize()
 		return map(lambda learner: learner.markov, self.learners)
 
+class MarkovGrapher:
+	""" Helper class to graph the structure of a Markov object.
+	Requires gven. """
+	def graph(self, markov):
+		from gvgen import GvGen
+
+		gv = GvGen()
+		items = []
+
+		gv.styleAppend("edge", "fontname", "gotham")
+		gv.styleAppend("edge", "fontcolor", "#999999")
+		gv.styleAppend("edge", "color", "#999999")
+
+		gv.styleAppend("node", "fontname", "gotham")
+		gv.styleAppend("node", "fontname", "gotham")
+		gv.styleAppend("node", "shape", "circle")
+
+		# first pass: add nodes
+		for n, node in enumerate(markov.nodes):
+			if type(node) == int:
+				node = miditopitch(node)
+			item = gv.newItem(str(node))
+			items.append(item)
+			gv.styleApply("node", item)
+
+		# second pass: add edges
+		for n, node in enumerate(markov.nodes):
+			edges = markov.edges[n]
+			for e in filter(lambda e: edges[e] > 0, range(len(edges))):
+				link = gv.newLink(items[n], items[e], "%.1f" % edges[e])
+				gv.styleApply("edge", link)
+				# gv.propertyAppend(link, "weight", edges[e])
+
+		gv.dot(sys.stderr)
