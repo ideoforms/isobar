cf8f89b9154d0e6fd97394559e3b58107b46901a
Daniel Jones <daniel@jones.org.uk>
2015-08-04 14:14:46 +0100

    MidiFileOut: take filename as init parameter; remove unused fields

--- isobar/io/midifile.py
+++ isobar/io/midifile.py
@@ -134,13 +134,11 @@ class MidiFileOut:
 	    Requires the MIDIUtil package:
 		https://code.google.com/p/midiutil/ """
 
-	def __init__(self, numtracks = 16):
+	def __init__(self, filename = "score.mid", num_tracks = 16):
 		from midiutil.MidiFile import MIDIFile
 
-		self.score = MIDIFile(numtracks)
-		self.track = 0
-		self.channel = 0
-		self.volume = 64
+		self.filename = filename
+		self.score = MIDIFile(num_tracks)
 		self.time = 0
 
 	def tick(self, tick_length):
@@ -155,10 +153,10 @@ class MidiFileOut:
 
 	def note_off(self, note = 60, channel = 0):
 		time = round(self.time, 5)
-		self.score.addNote(channel, channel, note, time, duration, 0)
+		self.score.addNote(channel, channel, note, time, 0, 0)
 
-	def writeFile(self, filename = "score.mid"):
-		fd = open(filename, 'wb')
+	def write(self):
+		fd = open(self.filename, 'wb')
 		self.score.writeFile(fd)
 		fd.close()
 
@@ -167,7 +165,7 @@ class PatternWriterMIDI:
 	    Requires the MIDIUtil package:
 		https://code.google.com/p/midiutil/ """
 
-	def __init__(self, numtracks = 1):
+	def __init__(self, filename = "score.mid", numtracks = 1):
 		from midiutil.MidiFile import MIDIFile
 
 		self.score = MIDIFile(numtracks)
@@ -175,30 +173,35 @@ class PatternWriterMIDI:
 		self.channel = 0
 		self.volume = 64
 
-	def addTrack(self, pattern, tracknumber = 0, trackname = "track", dur = 1.0):
+	def add_track(self, pattern, track_number = 0, track_name = "track", dur = 1.0):
 		time = 0
+
 		# naive approach: assume every duration is 1
 		# TODO: accept dicts or PDicts
 		try:
 			for note in pattern:
 				vdur = Pattern.value(dur)
 				if note is not None and vdur is not None:
-					self.score.addNote(tracknumber, self.channel, note, time, vdur, self.volume)
+					self.score.addNote(track_number, self.channel, note, time, vdur, self.volume)
 					time += vdur
 				else:
 					time += vdur
 		except StopIteration:
-			# a StopIteration exception means that an input pattern has been exhausted.
-			# catch it and treat the track as completed.
+			#------------------------------------------------------------------------
+			# a StopIteration exception means that an input pattern has been
+			# exhausted. catch it and treat the track as completed.
+			#------------------------------------------------------------------------
 			pass
 
-	def addTimeline(self, timeline):
+	def add_timeline(self, timeline):
+		#------------------------------------------------------------------------
 		# TODO: translate entire timeline into MIDI
 		# difficulties: need to handle degree/transpose params
 		#			   need to handle channels properly, and reset numtracks
+		#------------------------------------------------------------------------
 		pass
 
-	def writeFile(self, filename = "score.mid"):
+	def write(self, filename = "score.mid"):
 		fd = open(filename, 'wb')
 		self.score.writeFile(fd)
 		fd.close()
