7b1feb0e0576707fc012ce75954ecfcad1a51fe3
Daniel Jones <daniel@jones.org.uk>
2020-06-07 23:49:39 +0100

    Added constants/exceptions

--- isobar/exceptions.py
+++ isobar/exceptions.py
@@ -3,3 +3,9 @@ class DeviceNotFoundException (Exception):
     I/O device could not be found.
     """
     pass
+
+class TrackLimitReachedException (Exception):
+    """
+    No more tracks could be scheduled in this Timeline.
+    """
+    pass
