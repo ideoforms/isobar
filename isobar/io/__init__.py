3ecb20f4e943dd75b57449f0869b9530a59a629c
Daniel Jones <daniel@jones.org.uk>
2020-06-07 23:49:55 +0100

    Add DummyOutputDevice

--- isobar/io/__init__.py
+++ isobar/io/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0"
 __author__ = "Daniel Jones <http://www.erase.net/>"
 
+from .dummy import DummyOutputDevice
 from .midi import MidiIn, MidiOut
 from .midifile import MidiFileIn, MidiFileOut, PatternWriterMIDI
 from .osc import OSCOut
--- isobar/io/dummy/output.py
+++ isobar/io/dummy/output.py
@@ -1,25 +1,24 @@
 import logging
+from ..output import OutputDevice
 
 log = logging.getLogger(__name__)
 
-class DummyOutputDevice:
+class DummyOutputDevice(OutputDevice):
     def __init__(self):
         """
         Dummy output device.
         """
         self.current_time = 0.0
+        self.events = []
 
     def tick(self, tick_duration):
         self.current_time += tick_duration
 
     def note_on(self, note=60, velocity=64, channel=0):
-        pass
+        self.events.append([self.current_time, "note_on", note, velocity, channel])
 
     def note_off(self, note=60, channel=0):
-        pass
-
-    def all_notes_off(self, channel=0):
-        pass
+        self.events.append([self.current_time, "note_off", note, channel])
 
     def control(self, control=0, value=0, channel=0):
-        pass
+        self.events.append([self.current_time, "control", value, channel])
