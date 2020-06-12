61f45472b6cd6ef9caddb4e42da8aab50e6154e7
Daniel Jones <daniel@jones.org.uk>
2020-06-12 14:40:25 +0100

    Simplify install_requires

--- setup.py
+++ setup.py
@@ -20,8 +20,7 @@ setup(
     author_email = 'dan-isobar@erase.net',
     url = 'https://github.com/ideoforms/isobar',
     packages = find_packages(),
-    # install_requires = ['python-osc', 'python-rtmidi', 'midiutil', 'mido'],
-    install_requires = ['python-osc', 'midiutil', 'mido'],
+    install_requires = ['python-osc', 'mido'],
     keywords = ('sound', 'music', 'composition'),
     classifiers = [
         'Topic :: Multimedia :: Sound/Audio',
