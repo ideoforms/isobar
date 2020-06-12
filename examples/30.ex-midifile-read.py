42e10e0d3ce52a781f3b7dc4be5e24b3ada18b02
Daniel Jones <daniel@jones.org.uk>
2020-06-12 14:40:44 +0100

    Examples: MIDI file read/write

---
+++ examples/30.ex-midifile-read.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+#------------------------------------------------------------------------
+# ex-midifile-read:
+#
+# Example of reading from a MIDI file in real time.
+#------------------------------------------------------------------------
+
+import isobar as iso
+from isobar.io import MidiFileIn
+
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")
+
+parser = argparse.ArgumentParser(description="Read and play a .mid file")
+parser.add_argument("filename", type=str, help="File to load (.mid)")
+args = parser.parse_args()
+
+pattern = iso.PDict()
+pattern.load(args.filename)
+print(pattern["note"])
+
+timeline = iso.Timeline()
+timeline.schedule(pattern)
+timeline.run()
--- examples/31.ex-midifile-write.py
+++ examples/31.ex-midifile-write.py
@@ -10,21 +10,28 @@ import isobar as iso
 from isobar.io import MidiFileOut
 
 import logging
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")
 
-logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s] %(message)s")
-
-chords = iso.PSequence([ [0, 2, 4], [1, 3, 5], [2, 4, 6], [3, 5, 7], [4, 6, 8] ], 1)
-chords = iso.PSequence([0, 1, 2, 3], 1)
+key = iso.Key("C", "major")
 
 filename = "output.mid"
 output = MidiFileOut(filename)
 
-timeline = iso.Timeline(120, output)
+timeline = iso.Timeline(iso.MAX_CLOCK_RATE, output_device=output)
 timeline.sched({
-    iso.EVENT_NOTE: chords,
-    iso.EVENT_DURATION: 0.25,
-    iso.EVENT_GATE: 1.0
+    iso.EVENT_NOTE: iso.PDegree(iso.PSequence([ 0, 1, 2, 4 ], 4), key),
+    iso.EVENT_OCTAVE: 5,
+    iso.EVENT_GATE: iso.PSequence([ 0.5, 1, 2, 1 ]),
+    iso.EVENT_AMPLITUDE: iso.PSequence([ 100, 80, 60, 40], 4),
+    iso.EVENT_DURATION: 1.0
 })
+timeline.sched({
+    iso.EVENT_NOTE: iso.PDegree(iso.PSequence([ 7, 6, 4, 2 ], 4), key),
+    iso.EVENT_OCTAVE: 6,
+    iso.EVENT_GATE: 1,
+    iso.EVENT_AMPLITUDE: iso.PSequence([ 80, 70, 60, 50], 4),
+    iso.EVENT_DURATION: 1.0
+}, delay=0.5)
 timeline.run()
 output.write()
 
