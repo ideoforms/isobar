32ff3bc8f1671e16c1f0198d5388964cd6517958
Daniel Jones <daniel@jones.org.uk>
2020-06-13 11:11:27 +0100

    Restore rtmidi requirement

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author_email = 'dan-isobar@erase.net',
     url = 'https://github.com/ideoforms/isobar',
     packages = find_packages(),
-    install_requires = ['python-osc', 'mido', 'rtmidi'],
+    install_requires = ['python-osc', 'mido', 'python-rtmidi'],
     keywords = ('sound', 'music', 'composition'),
     classifiers = [
         'Topic :: Multimedia :: Sound/Audio',
