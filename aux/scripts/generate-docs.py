03944a429e32029265aaef61be3e73c5366913cc
Daniel Jones <daniel@jones.org.uk>
2020-07-13 23:57:03 +0100

    Add support for generating markdown docs

--- aux/scripts/generate-docs.py
+++ aux/scripts/generate-docs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 #------------------------------------------------------------------------
 # Generate isobar README from template and current class docstrings
@@ -7,6 +7,11 @@
 import re
 import os
 import glob
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--markdown", action="store_true")
+args = parser.parse_args()
 
 example_files = list(sorted(glob.glob("examples/*ex-*.py")))
 
@@ -15,14 +20,23 @@ for example in example_files:
 	example = os.path.basename(example)
 	v_examples += "* [%s](examples/%s)\n" % (example, example)
 
+if not args.markdown:
+	print(v_examples)
+
 files = [ "core", "scalar", "sequence", "chance", "tonal", "static", "fade", "markov", "lsystem", "warp" ]
 regexp = '^\s+""" ?(P\w+): (.+?)(\s*"""\s*)?$'
 fmt = "    %-20s - %s\n"
 
-v_classes = ""
 for prefix in files:
-	contents = file("isobar/pattern/%s.py" % prefix, "r").readlines()
-	v_classes += "    %s (%s.py)\n" % (prefix.upper(), prefix)
+	contents = open("isobar/pattern/%s.py" % prefix, "r").readlines()
+	if args.markdown:
+		print("## %s" % prefix.title())
+		print("View source: [%s.py](https://github.com/ideoforms/isobar/tree/master/isobar/pattern/%s.py)" % (prefix, prefix))
+		print()
+		print("| Class | Function |")
+		print("|-|-|")
+	else:
+		print("    %s (%s.py)" % (prefix.upper(), prefix))
 	for line in contents:
 		line = line.rstrip()
 		
@@ -30,8 +44,10 @@ for prefix in files:
 		if match:
 			classname = match.group(1)
 			docs = match.group(2)
-			v_classes += fmt % (classname, docs)
-	v_classes += "\n"
 
-print(v_examples)
-print(v_classes)
+			if args.markdown:
+				print("| %s | %s |" % (classname, docs))
+			else:
+				print(fmt % (classname, docs), end="")
+	print()
+
