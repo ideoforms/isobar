a2c46f18d37e2145e2b1b408a5b2d91357b3bfa4
Daniel Jones <daniel@jones.org.uk>
2020-10-07 14:47:07 +0100

    Update LICENSE; bump version

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # MIT License
 
-Copyright (c) 2019 Daniel John Jones
+Copyright (c) 2011-2020 Daniel John Jones
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- setup.py
+++ setup.py
@@ -7,22 +7,23 @@ from setuptools import setup, find_packages
 #------------------------------------------------------------------------
 try:
     import pypandoc
+
     long_description = pypandoc.convert('README.md', 'rst')
 except:
     long_description = None
 
 setup(
-    name = 'isobar',
-    version = '0.0.1',
-    description = 'A Python library to express and manipulate musical patterns',
-    long_description = long_description,
-    author = 'Daniel Jones',
-    author_email = 'dan-isobar@erase.net',
-    url = 'https://github.com/ideoforms/isobar',
-    packages = find_packages(),
-    install_requires = ['python-osc', 'mido', 'python-rtmidi'],
-    keywords = ('sound', 'music', 'composition'),
-    classifiers = [
+    name='isobar',
+    version='0.1.0',
+    description='A Python library to express and manipulate musical patterns',
+    long_description=long_description,
+    author='Daniel Jones',
+    author_email='dan-isobar@erase.net',
+    url='https://github.com/ideoforms/isobar',
+    packages=find_packages(),
+    install_requires=['python-osc', 'mido', 'python-rtmidi'],
+    keywords=('sound', 'music', 'composition'),
+    classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Artistic Software',
         'Development Status :: 4 - Beta',
