de2b7db3185395e42f0a1c6881e00169c8dc432b
Daniel Jones <daniel@jones.org.uk>
2020-10-07 11:53:42 +0100

    Update unit tests for Control events

--- tests/test_timeline_event_control.py
+++ tests/test_timeline_event_control.py
@@ -46,22 +46,37 @@ def test_event_control_linear_interpolation(dummy_timeline):
     assert len(dummy_timeline.output_device.events) == (dummy_timeline.ticks_per_beat * 1.5) + 1
     assert expected_series == pytest.approx(values, rel=0.01)
 
+def test_event_control_linear_interpolation_zero_duration(dummy_timeline):
+    control_series = iso.PSequence([0, 1])
+    duration_series = iso.PSequence([1, 0])
+    dummy_timeline.ticks_per_beat = 10
+    dummy_timeline.schedule({
+        iso.EVENT_CONTROL: 0,
+        iso.EVENT_VALUE: control_series,
+        iso.EVENT_DURATION: duration_series,
+        iso.EVENT_CHANNEL: 9
+    }, interpolate=iso.INTERPOLATION_LINEAR, count=4)
+    dummy_timeline.run()
+    expected_series = [0.1 * n for n in range(0, 11)] + [0.1 * n for n in range(1, 11)]
+    values = [event[3] for event in dummy_timeline.output_device.events]
+    assert expected_series == pytest.approx(values, rel=0.0000001)
+
 def test_event_control_cosine_interpolation(dummy_timeline):
     """
     Linear interpolation between control points.
     """
-    alternator = iso.PSequence([ 0, 1 ])
+    alternator = iso.PSequence([0, 1])
     dummy_timeline.ticks_per_beat = 10
     dummy_timeline.schedule({
         iso.EVENT_CONTROL: 0,
         iso.EVENT_VALUE: alternator,
         iso.EVENT_CHANNEL: 9
-    }, interpolate=iso.INTERPOLATION_COSINE, count=4)
+    }, interpolate=iso.INTERPOLATION_COSINE, count=3)
     dummy_timeline.run()
 
     expected_series = [
         0.5 * (1.0 - math.cos(math.pi * n / dummy_timeline.ticks_per_beat))
-            for n in range(3 * dummy_timeline.ticks_per_beat + 1)
+        for n in range(2 * dummy_timeline.ticks_per_beat + 1)
     ]
     values = [event[3] for event in dummy_timeline.output_device.events]
     assert expected_series == pytest.approx(values, rel=0.000001)
