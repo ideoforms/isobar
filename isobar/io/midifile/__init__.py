d3039934d2a1e87bf9fe27df4fec277ceeb40137
Daniel Jones <daniel@jones.org.uk>
2020-10-08 11:48:46 +0100

    Add class alias for backwards-compatibility

--- isobar/io/midifile/__init__.py
+++ isobar/io/midifile/__init__.py
@@ -2,3 +2,6 @@ from .input import MidiFileInputDevice
 from .output import MidiFileOutputDevice, PatternWriterMIDI
 
 __all__ = ["MidiFileInputDevice", "MidiFileOutputDevice", "PatternWriterMIDI"]
+
+# Add class alias for backwards-compatibility
+MidiFileOut = MidiFileOutputDevice
