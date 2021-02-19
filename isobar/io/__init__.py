76f07f5fd244a0324c034ee070daed46d47c9476
Daniel Jones <daniel@jones.org.uk>
2021-02-19 11:57:49 +0000

    Add CV support

--- isobar/io/__init__.py
+++ isobar/io/__init__.py
@@ -6,6 +6,7 @@ from .dummy import DummyOutputDevice
 from .midi import MidiInputDevice, MidiOutputDevice, get_midi_output_names, get_midi_input_names
 from .midifile import MidiFileInputDevice, MidiFileOutputDevice, PatternWriterMIDI
 from .osc import OSCOutputDevice
+from .cv import CVOutputDevice, get_cv_output_devices
 from .socketio import SocketIOOutputDevice
 from .signalflow import SignalFlowOutputDevice
 from .midinote import MidiNote
@@ -15,4 +16,5 @@ __all__ = ["OutputDevice", "DummyOutputDevice", "MidiInputDevice", "MidiOutputDe
 __all__ += ["get_midi_output_names", "get_midi_input_names"]
 __all__ += ["MidiFileInputDevice", "MidiFileOutputDevice", "PatternWriterMIDI"]
 __all__ += ["OSCOutputDevice", "SocketIOOutputDevice", "SignalFlowOutputDevice",
-            "MidiNote", "SuperColliderOutputDevice"]
+            "MidiNote", "SuperColliderOutputDevice", "CVOutputDevice"]
+__all__ += ["get_cv_output_devices"]
---
+++ isobar/io/cv/__init__.py
@@ -0,0 +1,3 @@
+from .output import CVOutputDevice, get_cv_output_devices
+
+__all__ = ["CVOutputDevice", "get_cv_output_devices"]
