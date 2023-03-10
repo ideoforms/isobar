e3e64e6e1296831f491ab30712360a56100f6c89
Daniel Jones <daniel@jones.org.uk>
2023-03-10 20:07:44 +0000

    Shared changes

--- aux/scripts/pull-descriptions.py
+++ aux/scripts/pull-descriptions.py
@@ -9,53 +9,113 @@ import os
 import glob
 import argparse
 
-# Look for arguments to output as markdown
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--markdown", action="store_true")
-args = parser.parse_args()
-
-# Get all pattern files, excluding __init__'s
-pattern_files = list(sorted(glob.glob("isobar/pattern/[!_]*.py", recursive=True)))
-
-for fname in pattern_files:
-    # Output file name (DEPRICATED, done by generate-docs)
-    basename = os.path.basename(fname)
-    #if args.markdown:
-    #    print("## %s" % basename[:-3].title())
-    #    print("View source: [%s](https://github.com/ideoforms/isobar/tree/master/isobar/pattern/%s)\n" % (basename, basename))
-    #else:
-    #    print("    %s (%s)" % (basename[:-3].title(), basename))
-
-    contents = open(fname, "r").read()
-
-    classregex = 'class\s[\w():]+\s+"""((?!""").)*"""'
-    cmatch = re.search(classregex, contents, re.S)
-
-    # Loop through for each class in the file
-    while cmatch != None:
-        name = re.search('(?<=class\s)[^(:]+', cmatch.group(), re.S).group()
-        desc = re.search('(?<=""")((?!""").)*', cmatch.group(), re.S).group()
-        # Format whitespace for easier output
-        desc = re.sub("\n[^\S\r\n]+", " ", desc)
-        desc = re.sub("\n\s","\n", desc).strip()
-
-        if args.markdown:
-            # Gather all information
-            mdContent = []
-            mdContent.append("## %s\n" % name)
-            mdContent.append(desc)
-            # Output to a file
-            newfile = open("zzz/%s.md" % (name), "w")
-            newfile.write("\n".join(mdContent))
-            newfile.close()
-        else:
-            print(name)
-            print(desc)
+def main(args):
+    class_data = parse_class_data(args)
+    if args.generate_index:
+        generate_index(class_data)
+    else:
+        for file_dict in class_data:
+            print(file_dict["name"])
+            for class_dict in file_dict["classes"]:
+                print(class_dict)
             print()
-        # Crop this section out, look for a new match
-        contents = contents[cmatch.end():]
+        
+
+def generate_index(class_data):
+    """
+    Iterates over each of the source files indexed in `class_data`, and outputs
+    a single page of Markdown which is the documentation index.
+    """
+    contents = ""
+    for file_dict in class_data:
+        contents += "## %s\n" % file_dict["name"].title()
+        contents += "View source: [%s.py](https://github.com/ideoforms/isobar/tree/master/isobar/pattern/%s.py)\n" % (file_dict["name"], file_dict["name"])
+        contents += "| Class | Function |\n"
+        contents += "|-|-|\n"
+        for class_dict in file_dict["classes"]:
+            contents += "| [%s](%s/%s.md) | %s |\n" % (class_dict["classname"],
+                                               file_dict["name"],
+                                               class_dict["classname"].lower(),
+                                               class_dict["short_description"])
+        contents += "|-|-|\n"
+    print(contents)
+
+def parse_class_data(args):
+    # Get all pattern files, excluding __init__'s
+    pattern_files = list(sorted(glob.glob("isobar/pattern/[!_]*.py", recursive=True)))
+
+    allDicts = []
+
+    for fname in pattern_files:
+        basename = os.path.basename(fname)
+        if args.markdown:
+            # Check if folder exists
+            if not os.path.exists("docs/patterns/%s" % basename[:-3].title()):
+                # Make the folder
+                os.mkdir("docs/patterns/%s" % basename[:-3].title())
+            #print("## %s" % basename[:-3].title())
+            #print("View source: [%s](https://github.com/ideoforms/isobar/tree/master/isobar/pattern/%s)\n" % (basename, basename))
+        else:
+            print("    %s (%s)" % (basename[:-3].title(), basename))
+
+        # Make a new dict for this file
+        fileDict = {
+            "name" : basename[:-3],
+            "classes" : []
+        }
+
+        contents = open(fname, "r").read()
+
+        classregex = 'class\s[\w():\s]+"""((?!""").)*"""'
         cmatch = re.search(classregex, contents, re.S)
 
-    # Class seperator
-    if not args.markdown:
-        print("---\n")
+        # Loop through for each class in the file
+        while cmatch != None:
+            name = re.search('(?<=class\s)[^(:]+', cmatch.group(), re.S).group()
+            desc = re.search('(?<=""")((?!""").)*', cmatch.group(), re.S).group()
+            # Format whitespace for easier output
+            output = None
+            desc = re.sub("\n[^\S\r\n]+", " ", desc)
+            desc = re.sub("\n\s","\n", desc).strip()
+
+            # Strip out class
+            contents = contents[cmatch.end():]
+
+            # Crop this section out, look for the init statement
+            arguments = None
+            cmatch = re.search('^\s*def __init__\(.+?\):', contents, re.S)
+            if (cmatch):
+                # Move to this index and search again for arg comments
+                contents = contents[cmatch.end():]
+                cmatch = re.search('^\s*""".+?"""', contents, re.S)
+                if (cmatch):
+                    # Arg comments found, format
+                    arguments = cmatch.group()
+
+            # Gather all information into dict
+            classDict = {
+                "classname" : name,
+                "short_description" : desc,
+                "long_description" : desc,
+                "example_output" : output,
+                "arguments" : arguments
+            }
+            fileDict["classes"].append(classDict)
+
+            # Crop this section out, look for a new match
+            cmatch = re.search(classregex, contents, re.S)
+
+        # Append file dict to full dict list
+        allDicts.append(fileDict)
+
+    # Return dict list
+    return allDicts
+
+if __name__ == "__main__":
+    # Look for arguments to output as markdown
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--markdown", action="store_true")
+    parser.add_argument("--generate-index", action="store_true")
+    args = parser.parse_args()
+
+    main(args)
