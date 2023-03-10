1129f86825fe9e7ed4f0f3d2a194140b15ebe13a
Daniel Jones <daniel@jones.org.uk>
2023-03-10 18:56:49 +0000

    generate-docs: Add links to Markdown output

--- aux/scripts/generate-docs.py
+++ aux/scripts/generate-docs.py
@@ -9,45 +9,49 @@ import os
 import glob
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--markdown", action="store_true")
-args = parser.parse_args()
+def main(args):
+    example_files = list(sorted(glob.glob("examples/*ex-*.py")))
 
-example_files = list(sorted(glob.glob("examples/*ex-*.py")))
+    v_examples = ""
+    for example in example_files:
+        example = os.path.basename(example)
+        v_examples += "* [%s](examples/%s)\n" % (example, example)
 
-v_examples = ""
-for example in example_files:
-	example = os.path.basename(example)
-	v_examples += "* [%s](examples/%s)\n" % (example, example)
+    if not args.markdown:
+        print(v_examples)
 
-if not args.markdown:
-	print(v_examples)
+    files = ["core", "scalar", "sequence", "chance", "tonal", "static", "fade", "markov", "lsystem", "warp"]
+    regexp = '^\s+""" ?(P\w+): (.+?)(\s*"""\s*)?$'
+    fmt = "    %-24s - %s\n"
 
-files = [ "core", "scalar", "sequence", "chance", "tonal", "static", "fade", "markov", "lsystem", "warp" ]
-regexp = '^\s+""" ?(P\w+): (.+?)(\s*"""\s*)?$'
-fmt = "    %-24s - %s\n"
+    for prefix in files:
+        contents = open("isobar/pattern/%s.py" % prefix, "r").readlines()
+        if args.markdown:
+            print("## %s" % prefix.title())
+            print("View source: [%s.py](https://github.com/ideoforms/isobar/tree/master/isobar/pattern/%s.py)" % (prefix, prefix))
+            print()
+            print("| Class | Function |")
+            print("|-|-|")
+        else:
+            print("    %s (%s.py)" % (prefix.upper(), prefix))
+        for line in contents:
+            line = line.rstrip()
 
-for prefix in files:
-	contents = open("isobar/pattern/%s.py" % prefix, "r").readlines()
-	if args.markdown:
-		print("## %s" % prefix.title())
-		print("View source: [%s.py](https://github.com/ideoforms/isobar/tree/master/isobar/pattern/%s.py)" % (prefix, prefix))
-		print()
-		print("| Class | Function |")
-		print("|-|-|")
-	else:
-		print("    %s (%s.py)" % (prefix.upper(), prefix))
-	for line in contents:
-		line = line.rstrip()
-		
-		match = re.search(regexp, line)
-		if match:
-			classname = match.group(1)
-			docs = match.group(2)
+            match = re.search(regexp, line)
+            if match:
+                classname = match.group(1)
+                docs = match.group(2)
 
-			if args.markdown:
-				print("| %s | %s |" % (classname, docs))
-			else:
-				print(fmt % (classname, docs), end="")
-	print()
+                if args.markdown:
+                    print("| [%s](%s/%s.md) | %s |" % (classname, prefix, classname.lower(), docs))
+                else:
+                    print(fmt % (classname, docs), end="")
+        print()
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--markdown", action="store_true")
+    args = parser.parse_args()
+
+    main(args)
