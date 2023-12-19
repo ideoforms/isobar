abb9bd23c2acdf0f2c499dc4fb5836b9be0b184b
Daniel Jones <daniel@jones.org.uk>
2023-12-19 20:38:17 +0000

    Add support for Ableton Link as clock source

--- isobar/timeline/__init__.py
+++ isobar/timeline/__init__.py
@@ -1,5 +1,6 @@
 from .timeline import Timeline
 from .track import Track
 from .clock import Clock, DummyClock
+from .clock_link import AbletonLinkClock
 
-__all__ = ["Timeline", "Track", "Clock", "DummyClock"]
+__all__ = ["Timeline", "Track", "Clock", "DummyClock", "AbletonLinkClock"]
