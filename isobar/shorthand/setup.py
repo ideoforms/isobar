3a4a18362d772d9c19429c04cfa824431d0a2861
Daniel Jones <daniel@jones.org.uk>
2023-12-22 20:47:12 +0000

    Add shorthand structures

---
+++ isobar/shorthand/setup.py
@@ -0,0 +1,59 @@
+from signalflow import *
+from ..io.midi import MidiOutputDevice
+from ..io.signalflow import SignalFlowOutputDevice
+from ..timeline import Timeline
+from .. import ALL_EVENT_PARAMETERS
+
+graph = AudioGraph()
+signalflow_output_device = SignalFlowOutputDevice(graph)
+signalflow_output_device.added_latency_seconds = 0.05
+timeline = Timeline(120, signalflow_output_device)
+midi_output_device = MidiOutputDevice()
+timeline.add_output_device(midi_output_device)
+timeline.ignore_exceptions = True
+timeline.background()
+
+
+def track(name, **kwargs):
+    global timeline
+
+    #--------------------------------------------------------------------------------
+    # Unflatten the params list.
+    # This has some perils (e.g. 'amp' is used as an Event keyword but is 
+    # also often used as a Patch parameter).
+    #--------------------------------------------------------------------------------
+    params = {}
+    for key in list(kwargs.keys()):
+        if key not in ALL_EVENT_PARAMETERS:
+            params[key] = kwargs[key]
+            del kwargs[key]
+    
+    if params:
+        #--------------------------------------------------------------------------------
+        # This caused the track to not generate any events when params was null,
+        # not sure why
+        #--------------------------------------------------------------------------------
+        kwargs["params"] = params
+
+    #--------------------------------------------------------------------------------
+    # Automatically select the appropriate output device based on the event type.
+    #--------------------------------------------------------------------------------
+    if "patch" in kwargs:
+        output_device = signalflow_output_device
+    else:
+        output_device = midi_output_device
+
+    track = timeline.schedule(params=kwargs,
+                              name=name,
+                              replace=True,
+                              quantize=1,
+                              output_device=output_device)
+    #--------------------------------------------------------------------------------
+    # Evaluating a cell with a track() command with mute() appended to it causes
+    # the track to be silenced.
+    #
+    # Re-evaluating the cell without mute() should then unmute the track.
+    #--------------------------------------------------------------------------------
+    track.is_muted = False
+
+    return track
