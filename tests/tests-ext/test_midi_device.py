1747ec2f0e7f93eacf74d5c693670c654f4973f7
Piotr Sakowski <piotereks@gmail.com>
2024-03-16 22:01:30 +0100

    FlieOut - MidiFileOutputDevice (skip super_init parm) - test case

--- tests/tests-ext/test_midi_device.py
+++ tests/tests-ext/test_midi_device.py
@@ -1,5 +1,5 @@
-from isobar_ext.io import  MidiFileOutputDevice
-
+from isobar_ext import FileOut, MidiFileOutputDevice
+from pathlib import Path
 
 def test_get_channel_track():
     test_midi_out_device = MidiFileOutputDevice(filename='test_output_file')
@@ -49,3 +49,20 @@ def test_get_channel_track():
     test_midi_out_device.get_channel_track(channel=5, src_track_idx=2)
     assert test_midi_out_device.channel_track == [2, 3, 4, 5, 5]
     assert test_midi_out_device.tgt_track_idxs == [1, 2, 2, 1, 2]
+
+# from isobar_ext import *
+# import mido
+# from tracker.app.isobar_fixes import *
+# from tracker.app.midi_dev import *
+
+
+def test_wrk_midi_out():
+    """
+    This test check fix for FileOut, which has broken because trying to open midi output device 2 times.
+    """
+    device_name = 'Microsoft GS Wavetable Synth 0'
+    filename = (Path(__file__).resolve().parent / ".." / "saved_midi_files" / "xoutput.mid").resolve()
+    midi_out = FileOut(filename=filename, device_name=device_name, send_clock=True,
+                       virtual=False, ticks_per_beat=480)
+
+    assert midi_out
