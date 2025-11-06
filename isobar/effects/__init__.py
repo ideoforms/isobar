d3cf9a23e1ac8a3b112f08875638e0ed65d06b07
Daniel Jones <daniel@jones.org.uk>
2025-11-06 20:13:30 +0000

    NoteEffect: Add apply_chained_effects for effects sequences; add support for echo inserts

--- isobar/effects/__init__.py
+++ isobar/effects/__init__.py
@@ -1,3 +1,5 @@
 from .note_effect import NoteEffect, NoteEffectEcho, NoteEffectTranspose, NoteEffectScale
 
-from .note_effect import fxecho, fxtranspose, fxscale
+from .note_effect import NoteEffectEcho as fxecho
+from .note_effect import NoteEffectTranspose as fxtranspose
+from .note_effect import NoteEffectScale as fxscale
--- isobar/effects/note_effect.py
+++ isobar/effects/note_effect.py
@@ -4,24 +4,34 @@ from ..timelines.midi_note import MidiNoteInstance
 class NoteEffect:
     def __init__(self):
         self.track = None
+        self.effects = []
 
     def __gt__(self, other):
         if isinstance(other, NoteEffect):
-            return (self.track > other)
+            if self.track:
+                return (self.track > other)
+            else:
+                self.effects.append(other)
+                return self
         else:
             raise NotImplementedError("Cannot compare NoteEffect with %s" % type(other))
 
+    def apply_chained_effects(self, note: MidiNoteInstance):
+        for effect in self.effects:
+            effect.apply(note)
 
 class NoteEffectEcho (NoteEffect):
-    def __init__(self, active=1, delay=0.5, count=1, scale=1.0):
+    def __init__(self, active=1, delay=0.5, count=1, scale=1.0, insert: NoteEffect = None):
         super().__init__()
         self.active = Pattern.pattern(active)
         self.delay = Pattern.pattern(delay)
         self.count = Pattern.pattern(count)
         self.scale = Pattern.pattern(scale)
+        self.insert = Pattern.pattern(insert)
 
     def apply(self, note: MidiNoteInstance):
         active = Pattern.value(self.active)
+        insert = Pattern.value(self.insert)
         if not active:
             return
         
@@ -30,7 +40,10 @@ class NoteEffectEcho (NoteEffect):
             note_copy = note.copy()
             note_copy.timestamp += Pattern.value(self.delay * (i + 1))
             note_copy.amplitude = int(round(note_copy.amplitude * Pattern.value(self.scale)))
+            if insert is not None:
+                insert.apply(note_copy)
             note_copy.origin = self
+            self.apply_chained_effects(note_copy)
             note.track.schedule_note(note_copy)
 
 class NoteEffectTranspose(NoteEffect):
@@ -59,6 +72,3 @@ class NoteEffectScale(NoteEffect):
 
         note.amplitude = int(round(note.amplitude * Pattern.value(self.scale)))
 
-fxecho = NoteEffectEcho
-fxtranspose = NoteEffectTranspose
-fxscale = NoteEffectScale
