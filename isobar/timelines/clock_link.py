ca3469bb913beff332eb2a253bcfe6477eace3f8
Daniel Jones <daniel@jones.org.uk>
2026-03-05 23:18:46 +0000

    AbletonLinkClock: Start/stop sync support

--- isobar/timelines/clock_link.py
+++ isobar/timelines/clock_link.py
@@ -29,17 +29,17 @@ class AbletonLinkClock (Clock):
         def start_stop_callback(is_starting):
             logger.debug("Link: Start/Stop callback: is_starting=%s" % is_starting)
             if is_starting:
-                self.running = True
-                
+                self.clock_target.start()
             else:
-                self.running = False
+                self.clock_target.stop()
+                self.clock_target.reset()
 
         if start_stop_sync_enabled:
             self.link_client.setStartStopCallback(start_stop_callback)
             self.running = self.link_client.captureSessionState().isPlaying()
-            print("Start/Stop sync enabled, running: %s" % self.running)
+            logger.debug("Start/Stop sync enabled, is_running: %s" % self.running)
         else:
-            print("Start/Stop sync disabled, running")
+            logger.debug("Start/Stop sync disabled, running")
             self.running = True
 
         def tempo_changed_callback(tempo):
@@ -50,36 +50,39 @@ class AbletonLinkClock (Clock):
     def run(self):
         ticks_previous = None
         got_sync = False
+        self.running = True
 
-        while True:
-            if self.running:
-                link_state = self.link_client.captureSessionState()
-                link_time = self.link_client.clock().micros()
-                beats = link_state.beatAtTime(link_time, 4)
+        while self.running:
+            link_state = self.link_client.captureSessionState()
+            link_time = self.link_client.clock().micros()
+            beats = link_state.beatAtTime(link_time, 4)
 
-                ticks_current = int(beats * self.ticks_per_beat)
+            ticks_current = int(beats * self.ticks_per_beat)
+            if not got_sync:
+                #--------------------------------------------------------------------------------
+                # Start clock at next 4-beat boundary (next bar, assuming 4/4)
+                #--------------------------------------------------------------------------------
+                if ticks_current % (self.ticks_per_beat * 4) == 0:
+                    got_sync = True
+            if ticks_previous is None or ticks_current > ticks_previous:
+                if ticks_previous is None:
+                    delta_ticks = 1
+                else:
+                    #--------------------------------------------------------------------------------
+                    # Under system load, multiple ticks may have passed since our last timestamp was
+                    # received. In this case, send multiple ticks to the timeline to compensate.
+                    #--------------------------------------------------------------------------------
+                    delta_ticks = ticks_current - ticks_previous
+                ticks_previous = ticks_current
+
                 if not got_sync:
-                    #--------------------------------------------------------------------------------
-                    # Start clock at next 4-beat boundary (next bar, assuming 4/4)
-                    #--------------------------------------------------------------------------------
-                    if ticks_current % (self.ticks_per_beat * 4) == 0:
-                        got_sync = True
-                if ticks_previous is None or ticks_current > ticks_previous:
-                    if ticks_previous is None:
-                        delta_ticks = 1
-                    else:
-                        #--------------------------------------------------------------------------------
-                        # Under system load, multiple ticks may have passed since our last timestamp was
-                        # received. In this case, send multiple ticks to the timeline to compensate.
-                        #--------------------------------------------------------------------------------
-                        delta_ticks = ticks_current - ticks_previous
-                    ticks_previous = ticks_current
-
-                    if not got_sync:
-                        continue
-                    for n in range(delta_ticks):
-                        self.clock_target.tick()
+                    continue
+                for n in range(delta_ticks):
+                    self.clock_target.tick()
             time.sleep(0.0001)
+
+    def stop(self):
+        self.running = False
 
     def get_tempo(self):
         link_state = self.link_client.captureSessionState()
