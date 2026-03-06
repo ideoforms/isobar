699aa28eac6fe8448fe8d299afb5c64538bc4865
Daniel Jones <daniel@jones.org.uk>
2026-03-06 19:07:32 +0000

    Metronome: Allow configurable midi_channel

--- isobar/timelines/__init__.py
+++ isobar/timelines/__init__.py
@@ -5,5 +5,6 @@ from .automation import Automation
 from .clock import Clock, DummyClock
 from .clock_link import AbletonLinkClock
 from .midi_note import MidiNoteInstance
+from .metronome import Metronome, MetronomeConfig
 
 __all__ = ["Timeline", "Track", "LFO", "Automation", "Clock", "DummyClock", "AbletonLinkClock", "MidiNoteInstance", "Metronome", "MetronomeConfig"]
