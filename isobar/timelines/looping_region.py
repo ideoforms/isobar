8e9a23501159e81c8a2e16328743bc39347ce080
Daniel Jones <daniel@jones.org.uk>
2026-09-06 18:09:37 +0100

    LoopingRegion: Add to_dict/from_dict

--- isobar/timelines/looping_region.py
+++ isobar/timelines/looping_region.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 if TYPE_CHECKING:
     from ..timelines.track import Track
 
@@ -8,8 +8,24 @@ class LoopingRegion:
     def __init__(self,
                  track: Track,
                  start_time: float,
-                 end_time: float):
+                 end_time: float,
+                 loop: Optional[bool] = True):
         self.track = track
         self.start_time = start_time
         self.end_time = end_time
-        self.loop = True
+        self.loop = loop
+
+    def to_dict(self):
+        return {
+            "track": self.track,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "loop": self.loop
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> LoopingRegion:
+        return cls(track=data["track"],
+                   start_time=data["start_time"],
+                   end_time=data["end_time"],
+                   loop=data.get("loop", True))
